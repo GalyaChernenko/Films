e6910ed37cdb07243341554cb7965ed8e83d47fe
Chernenko <galka8@inbox.ru>
2023-02-19 20:50:50 +0200

    Change All

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class FilmsManagerTest {
+import org.junit.jupiter.api.Test;
+
+public class FilmsManagerTest {
+    @Test
+
+    public void test() {
+        FilmsManager manager = new FilmsManager();
+
+        manager.addFilm("FilmI");
+        manager.addFilm("FilmII");
+        manager.addFilm("FilmIII");
+
+        String[] expected = {"FilmI", "FilmII", "FilmIII"};
+        String[] actual = manager.findAll();
+
+
+    }
 }
